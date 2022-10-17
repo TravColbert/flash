92fdfa913d7cb9cb85630e87f277865e68c7f749
Trav Colbert <trav.colbert@gmail.com>
2022-10-17 15:41:38 -0700

    initial commit

---
+++ controllers/pet/index.js
@@ -0,0 +1,41 @@
+'use strict'
+
+/**
+ * Module dependencies.
+ */
+
+var db = require('../../db');
+
+exports.before = function(req, res, next){
+  var pet = db.pets[req.params.pet_id];
+  if (!pet) return next('route');
+  req.pet = pet;
+  next();
+};
+
+exports.edit = function(req, res, next){
+  res.render('edit', { pet: req.pet });
+};
+
+// exports.delete = function(req, res, next) {
+//   searchRecord = db.pets.findIndex(pet => pet.id === req.pet.id);
+//   if (searchRecord === -1) return next('route');
+//   db.pets.splice(searchRecord, 1);
+//   res.redirect('/pets');
+// };
+
+exports.list = function(req, res, next){
+  verbose && console.log('HELLO?')
+  res.render('list', { pets: db.pets });
+};
+
+exports.show = function(req, res, next){
+  res.render('show', { pet: req.pet });
+};
+
+exports.update = function(req, res, next){
+  var body = req.body;
+  req.pet.name = body.pet.name;
+  res.message('Information updated!');
+  res.redirect('/pets/' + req.pet.id);
+};
