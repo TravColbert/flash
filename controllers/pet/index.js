4bdde7307d9437b8a0e2c22ce9bc7ce6fd3e7feb
tcolbert <tcolbert@bethel.jw.org>
2022-10-20 06:27:02 -0700

    add more modules

--- controllers/pet/index.js
+++ controllers/pet/index.js
@@ -1,41 +1,39 @@
-'use strict'
+"use strict";
 
 /**
  * Module dependencies.
  */
 
-var db = require('../../db');
+var db = require("../../db");
 
-exports.before = function(req, res, next){
+exports.before = function (req, res, next) {
   var pet = db.pets[req.params.pet_id];
-  if (!pet) return next('route');
-  req.pet = pet;
+  req.pet = !pet ? {} : pet;
   next();
 };
 
-exports.edit = function(req, res, next){
-  res.render('edit', { pet: req.pet });
+exports.edit = function (req, res, next) {
+  res.render("edit", { pet: req.pet });
 };
 
-// exports.delete = function(req, res, next) {
-//   searchRecord = db.pets.findIndex(pet => pet.id === req.pet.id);
-//   if (searchRecord === -1) return next('route');
-//   db.pets.splice(searchRecord, 1);
-//   res.redirect('/pets');
-// };
-
-exports.list = function(req, res, next){
-  verbose && console.log('HELLO?')
-  res.render('list', { pets: db.pets });
+exports.delete = function (req, res, next) {
+  searchRecord = db.pets.findIndex((pet) => pet.id === req.pet.id);
+  if (searchRecord === -1) return next("route");
+  db.pets.splice(searchRecord, 1);
+  res.redirect("/pets");
 };
 
-exports.show = function(req, res, next){
-  res.render('show', { pet: req.pet });
+exports.list = function (req, res, next) {
+  res.render("list", { pets: db.pets });
 };
 
-exports.update = function(req, res, next){
+exports.show = function (req, res, next) {
+  res.render("show", { pet: req.pet });
+};
+
+exports.update = function (req, res, next) {
   var body = req.body;
   req.pet.name = body.pet.name;
-  res.message('Information updated!');
-  res.redirect('/pets/' + req.pet.id);
+  res.message("Information updated!");
+  res.redirect("/pets/" + req.pet.id);
 };
